aaa27ecadc05ab1a92c7c10d17c7798cdc548127
toyosuke <toyosukevarubogu@gmail.com>
2020-12-14 14:07:43 +0900

    フォルダ区分け、それに伴う名前空間変更

---
+++ WpfApp1/Models/ChoiceGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    class ChoiceGroup
+    {
+        public string Name { set; get; }
+        public List<ChoiceExpression> Childs { set; get; }
+    }
+}
